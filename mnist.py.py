4a87aaf454f23b097506d4bd4c22de3851aed11c
sandeep-raj-saimon <36258508+sandeep-raj-saimon@users.noreply.github.com>
2019-04-01 13:02:08 +0530

    Update mnist.py.py

--- mnist.py.py
+++ mnist.py.py
@@ -1,38 +1,64 @@
 import numpy as np
-
 import tensorflow as tf
-old_v = tf.logging.get_verbosity()
-tf.logging.set_verbosity(tf.logging.ERROR)
+from keras.utils import to_categorical
 
-NUM_STEPS = 500
-MINIBATCH_SIZE = 50
-from tensorflow.examples.tutorials.mnist import input_data
+# global data
+count = 0
+BATCH_SIZE = 125
+NUM_STEPS = 1500
 
-data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+#load mnist data
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-x = tf.placeholder(tf.float32,[None,784])
-W = tf.Variable(tf.zeros([784,10]))
+# convert to one-hot encoding
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
-y_true = tf.placeholder(tf.float32,[None,10])
-y_pred = tf.matmul(x,W)
+# placeholder for input data
+x = tf.placeholder(tf.float32,shape=[None,784])
+y_actual = tf.placeholder(tf.float32,shape=[None,10])
 
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_pred,labels = y_true))
+# weights 
+w = tf.Variable(tf.zeros([784,10]))
+
+# predicted value
+y_pred = tf.matmul(x,w)
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_pred,labels = y_actual))
 gd_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 #Explanation for below code:
 #	tf.argmax returns index with largest value across axis of a tensorflow
 #	tf.equal accepts two tensors x and y and returns truth value (x==y)
 # 	in the below code indices of both y_pred and y_true are compared by tf.equal
-correct_mask = tf.equal(tf.argmax(y_pred,1),tf.argmax(y_true,1))
+correct_mask = tf.equal(tf.argmax(y_pred,1),tf.argmax(y_actual,1))
 accuracy = tf.reduce_mean(tf.cast(correct_mask,tf.float32))
 
+# generating next batch for training
+def next_batch(num):
+  global count
+  x,y = x_train[count:count+num],y_train[count:count+num]
+  count = (count+num)%len(x_train)
+  return x,y
+
 with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
+  
+  sess.run(tf.global_variables_initializer())
+  
+  for _ in range(NUM_STEPS):
+    
+    batch_xs,batch_ys = next_batch(BATCH_SIZE)
+    
+    batch_xs = batch_xs.reshape(BATCH_SIZE,784)
+    batch_ys = batch_ys.reshape(BATCH_SIZE,10)
+    
+    sess.run(gd_step,feed_dict={x:batch_xs,y_actual:batch_ys})
+  
+  x_test = x_test.reshape(10000,784)
+  y_test = y_test.reshape(10000,10)
+  
+  ans = sess.run(accuracy,feed_dict={x:x_test,y_actual:y_test})
 	
-	for _ in range(NUM_STEPS):
-		batch_xs,batch_ys = data.train.next_batch(MINIBATCH_SIZE)
-		sess.run(gd_step,feed_dict={x:batch_xs,y_true:batch_ys})
-	
-	ans = sess.run(accuracy,feed_dict={x:data.test.images,y_true:data.test.labels})
-	
-print("Accuracy : {:.4%}".format(ans*100))
+print("Accuracy : {:.2%}".format(ans))
+
+
