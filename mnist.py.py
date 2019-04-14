9108fafca552c1e03d7236dbf2cb5c2704184a15
sandeep-raj-saimon <36258508+sandeep-raj-saimon@users.noreply.github.com>
2019-04-14 09:03:05 +0530

    Update mnist.py.py

--- mnist.py.py
+++ mnist.py.py
@@ -1,64 +1,56 @@
 import numpy as np
+
 import tensorflow as tf
-from keras.utils import to_categorical
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-# global data
-count = 0
-BATCH_SIZE = 125
-NUM_STEPS = 1500
+NUM_STEPS = 1000
+MINIBATCH_SIZE = 100
+from tensorflow.examples.tutorials.mnist import input_data
 
-#load mnist data
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+data = input_data.read_data_sets("MNIST_data/", one_hot=True)
+LOG_DIR = 'E:\Learn_Data_Science_in_3_Months-master\Tensorflow'
 
-# convert to one-hot encoding
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+x = tf.placeholder(tf.float32,[None,784])
+W = tf.Variable(tf.zeros([784,10]))
 
-# placeholder for input data
-x = tf.placeholder(tf.float32,shape=[None,784])
-y_actual = tf.placeholder(tf.float32,shape=[None,10])
+y_true = tf.placeholder(tf.float32,[None,10])
+y_pred = tf.matmul(x,W)
 
-# weights 
-w = tf.Variable(tf.zeros([784,10]))
-
-# predicted value
-y_pred = tf.matmul(x,w)
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_pred,labels = y_actual))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y_pred,labels = y_true))
+tf.summary.scalar('cross_entropy',cross_entropy)
 gd_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 #Explanation for below code:
 #	tf.argmax returns index with largest value across axis of a tensorflow
 #	tf.equal accepts two tensors x and y and returns truth value (x==y)
 # 	in the below code indices of both y_pred and y_true are compared by tf.equal
-correct_mask = tf.equal(tf.argmax(y_pred,1),tf.argmax(y_actual,1))
+correct_mask = tf.equal(tf.argmax(y_pred,1),tf.argmax(y_true,1))
 accuracy = tf.reduce_mean(tf.cast(correct_mask,tf.float32))
+tf.summary.scalar('accuracy',accuracy)
 
-# generating next batch for training
-def next_batch(num):
-  global count
-  x,y = x_train[count:count+num],y_train[count:count+num]
-  count = (count+num)%len(x_train)
-  return x,y
+merged = tf.summary.merge_all()
 
 with tf.Session() as sess:
-  
-  sess.run(tf.global_variables_initializer())
-  
-  for _ in range(NUM_STEPS):
-    
-    batch_xs,batch_ys = next_batch(BATCH_SIZE)
-    
-    batch_xs = batch_xs.reshape(BATCH_SIZE,784)
-    batch_ys = batch_ys.reshape(BATCH_SIZE,10)
-    
-    sess.run(gd_step,feed_dict={x:batch_xs,y_actual:batch_ys})
-  
-  x_test = x_test.reshape(10000,784)
-  y_test = y_test.reshape(10000,10)
-  
-  ans = sess.run(accuracy,feed_dict={x:x_test,y_actual:y_test})
+
+	train_writer = tf.summary.FileWriter(LOG_DIR+'/train',graph = tf.get_default_graph())
+	test_writer = tf.summary.FileWriter(LOG_DIR+'/test',graph = tf.get_default_graph())
+	
+	sess.run(tf.global_variables_initializer())
+	
+	for i in range(NUM_STEPS):
+		batch_xs,batch_ys = data.train.next_batch(MINIBATCH_SIZE)
+		
+		summary,_ = sess.run([merged,gd_step],feed_dict={x:batch_xs,y_true:batch_ys})
+		train_writer.add_summary(summary,i)
+		sess.run(gd_step,feed_dict={x:batch_xs,y_true:batch_ys})
+		
+		if i %100 == 0:
+			acc,loss = sess.run([accuracy,cross_entropy],feed_dict={x:batch_xs,y_true:batch_ys})
+			
+			print("Iter : " + str(i) +" loss : "+ str(loss) + " training accuracy : " + str(acc))
+	
+	ans = sess.run(accuracy,feed_dict={x:data.test.images,y_true:data.test.labels})
+	
 	
 print("Accuracy : {:.2%}".format(ans))
-
-
